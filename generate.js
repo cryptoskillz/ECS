10eb86141b07ff60dde756a4da739d3aff7a071d
chris <chris@totemsoftware.co.uk>
2018-09-22 17:35:23 +0700

    added database support, fixed a bug with test net support and played with fromWif function

---
+++ generate.js
@@ -0,0 +1,93 @@
+/*
+
+Purpose of this branch
+
+Remove Block.io dependancy 
+Generate an address 
+Store the details in a SQL database, insecure I know we will be upgrading it later
+
+Notes
+	Why on earth do I have to specify the testnet in every method just let me call it once and be done with it.
+	I would assume it is wise to check the origin of the request here to stop potential dossing  I will read up on best practices. 
+	I have yet to read the best practices to store these in a safe manner (keys)
+	I will also look into striking back to FIAT (for funsies)
+	I will look into how to generate a bitcoin address
+	I will lastly look at using hardware wallets to control this (most likely ledger)
+*/
+
+
+//load express
+const express = require('express');
+//load body parser
+const bodyParser = require('body-parser');
+//load the bitcoin js files
+var bitcoin = require('bitcoinjs-lib');
+//load SQLlite (use any database you want or none)
+const sqlite3 = require('sqlite3').verbose();
+//init it
+const app = express();
+//set up the network we would like to connect to. in this case test net.
+const TestNet = bitcoin.networks.testnet
+
+//open a database connection
+let db = new sqlite3.Database('./db/db.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+//generate the key pair using the makeRandom functions (there a bunch of ways to make an address btw)
+let keyPair = bitcoin.ECPair.makeRandom({ network: TestNet });
+//extract the publickey
+//note: All the docs say you should use  keyPair.publicKey.toString('hex'); but whenever do that I get the following
+//		error "Error: Expected property "pubkey" of type ?isPoint, got String "03a53ff77bf5234a66f69ce23daea51c4f007669e6b41a6f4f57e0bacbcd93e7b1"
+//		so I do not use it.  I can only assume it was deperacted along the way as a lot of the docs seems to be.
+let publicKey = keyPair.publicKey
+//get the private key
+let privateKey = keyPair.toWIF();
+//debug
+//console.log(keyPair);
+//console.log(publicKey);
+//console.log(privateKey);
+
+//get an address from the keyPair we generated above. 
+let { address } = bitcoin.payments.p2pkh({ pubkey: publicKey,network: TestNet  });
+//debug
+//console.log(address);
+
+
+/*
+This is a test to make sure we can turn the private key back into the address.
+It does not appear to work at present, requires further research
+
+//rant: Why on earth do I have to specify the testnet parameter this way when its the other way everywhere else!
+let keyPair2 = bitcoin.ECPair.fromWIF(privateKey, TestNet)
+let publicKey2 = keyPair2.publicKey;
+//debug
+//console.log(keyPair2);
+//console.log(publicKey2);
+
+let { address2 } = bitcoin.payments.p2pkh({ pubkey: publicKey2,network: TestNet });
+console.log(address2);
+
+*/
+
+
+
+//store it in the database
+//note: Not 100% sure that we have to store the public kkey
+  db.run(`INSERT INTO keys(address,privatekey,publickey) VALUES(?,?,?)`, [address,privateKey,publicKey], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+  });
+
+
+//display it to the user
+console.log('Pay me f00l '+address)
+
+
+app.listen(3000, () => {
+});
