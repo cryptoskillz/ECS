12872073e3aa3f83dafc358730d17b7fd5fa49b4
chris <chris@totemsoftware.co.uk>
2018-09-23 17:05:13 +0700

    monitor bootstrap

---
+++ monitor.js
@@ -0,0 +1,18 @@
+/*
+
+This function monitors the block chain to see if out payments have been processed.
+
+*/
+
+
+//load express
+const express = require('express');
+//load body parser
+const bodyParser = require('body-parser');
+//init it
+const app = express();
+
+
+
+app.listen(3000, () => {
+});
