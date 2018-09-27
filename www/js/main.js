b19d7974bfbb24585322d881c6ec3513c4d34f8e
chris <chris@totemsoftware.co.uk>
2018-09-27 22:41:55 +0700

    code comments

--- www/js/main.js
+++ www/js/main.js
@@ -1,207 +1,115 @@
 jQuery(document).ready(function($)
 {
-	/*
-		This function deals with showing or hiding the cart.
 
-	*/
+	//set the server url.
 	var serverurl = "http://127.0.0.1:3000/";
-	var address = 'miFbYPyW8GQvJfKDAvkPAdzY1AX7Szdbos';
+	//set the address
+	var address = '';
+	//init price var
+	var productPrice = 0;
+	var cartWrapper = $('.cd-cart-container');
+	//set the cart wrapper
+	var productCustomization = $('.cd-customization'),
+	//set the cart
+	cart = $('.cd-cart'),
+	//set animating boolean
+	animating = false;
+
+	//call the server to get a btc address
 	$.get( serverurl+"api/address", function( data ) 
 	{
-	  data = jQuery.parseJSON( data );
-	  console.log(data.address);
-	  address = data.address;
-	  $('#bitcoinaddress').text(address);
-	  $('#bitcoinaddress').attr('href','bitcoin:'+address);
-	  $('#bitcoinqrcode').attr('src','https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl='+address);
-	  initCustomization(productCustomization);
-
+		//get the data
+	  	data = jQuery.parseJSON( data );
+	  	//console.log(data.address);
+	  	address = data.address;
+	  	//set the address
+	  	$('#bitcoinaddress').text(address);
+	  	//set the href of the bitcoin address
+	  	$('#bitcoinaddress').attr('href','bitcoin:'+address);
+	  	//generate the qr code
+	  	$('#bitcoinqrcode').attr('src','https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl='+address);
+	  	//init the product
 	});
 
+	initCustomization(productCustomization);
 
-
-	function toggleCart(bool) 
+	//check we have a cart
+	if( cartWrapper.length > 0 ) 
 	{
-		//always hide the address regardless of the toggle state as we never want to show this unless the 
-		//checkout button has been clicked
-		$('#bitcoinaddresswrapper').hide();
-		//check if the cart is open or not
-		var cartIsOpen = ( typeof bool === 'undefined' ) ? cartWrapper.hasClass('cart-open') : bool;
+		//store jQuery objects for later use
+		var cartBody = cartWrapper.find('.body')
+		var cartList = cartBody.find('ul').eq(0);
+		var cartTotal = cartWrapper.find('.checkout').find('span');
+		//console.log(cartTotal);
+		var cartTrigger = cartWrapper.children('.cd-cart-trigger');
+		var cartCount = cartTrigger.children('.count')
+		var addToCartBtn = $('.sr-add-to-cart');
+		var bitcoinback = cartWrapper.find('.bitcoinback');
 		
+		//add product to cart
+		addToCartBtn.on('click', function(event){
+			event.preventDefault();
+			addToCart($(this));
+		});
+
+		//open/close cart
+		cartTrigger.on('click', function(event){
+			event.preventDefault();
+			toggleCart();
+		});
+
+		//close cart when clicking on the .cd-cart-container::before (bg layer)
+		cartWrapper.on('click', function(event){
+			if( $(event.target).is($(this)) ) toggleCart(true);
+		});
+
+		//delete an item from the cart
+		cartList.on('click', '.delete-item', function(event){
+			event.preventDefault();
+			//remove the product
+			removeProduct($(event.target).parents('.product'));
+		});
+
+		//load btc address for payment
+		cartWrapper.on('click', '.checkout', function(event){
+			event.preventDefault();
+			bitcoinback.addClass('visible');
+			///$('#bitcoin-address-template').show();
+			$('#bitcoinaddresswrapper').show();
+			//hide the cart items
+			$('#cartlistitems').hide();
+		});
+
 		
-		if( cartIsOpen ) {
-			cartWrapper.removeClass('cart-open');
+		//copy the btc address to the clipboard
+		cartWrapper.on('click', '.bitcoinaddresscopy', function(event){
+			event.preventDefault();
+			/* Get the text field */
+  			var element = document.getElementById("bitcoinaddress");
+  			var $temp = $("<input>");
+  			$("body").append($temp);
+  			$temp.val($(element).text()).select();
+ 		 	document.execCommand("copy");
+  			//$temp.remove();
+
+
+		});
+
+		//back button is clicked so show items
+		cartWrapper.on('click', '#checkoutbitocoin', function(event){
+			event.preventDefault();
 			bitcoinback.removeClass('visible');
-			cartList.find('.deleted').remove();
-			bitcoinback.removeClass('visible');
-			$('#bitcoin-address-template').hide();
+			$('#bitcoinaddresswrapper').hide();
+			//$('#bitcoin-address-template').hide();
 			$('#cartlistitems').show();
-
-			setTimeout(function(){
-				cartBody.scrollTop(0);
-				//check if cart empty to hide it
-				if( Number(cartCount.find('li').eq(0).text()) == 0) cartWrapper.addClass('empty');
-			}, 500);
-		} else {
-			cartWrapper.addClass('cart-open');
-		}
-	}
-
-	function addToCart(trigger) {
-		var cartIsEmpty = cartWrapper.hasClass('empty');
-		//update cart product list
-		addProduct(trigger.data('price'),trigger.data('name'),trigger.data('id'));
-		
-		//update number of items 
-		updateCartCount(cartIsEmpty);
-		//update total price
-		updateCartTotal(trigger.data('price'), true);
-		//show cart
-		cartWrapper.removeClass('empty');
+		});
 
 	}
 
-	function addProduct(price,name,productId) 
+	function initCustomization(items) 
 	{
-		//get the product quantity
-		prodcutquantity= $('#productquantity').val();
-		//check if its set or not if not then set it 
-		if (prodcutquantity == undefined)
-			prodcutquantity = 0;
-		//increment it by 1
-		prodcutquantity++;
-		//empty  the car contents
-		cartList.empty();
-		//build the row html
-		//main product div
-		var prodcuthtml = '';
-		var prodcuthtml = prodcuthtml +'<li class="product">';
-		//product image
-		var prodcuthtml = prodcuthtml +'<div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div>';
-		//product details div
-		var prodcuthtml = prodcuthtml + '<div class="product-details">';
-		//product name
-		var prodcuthtml = prodcuthtml + '<h3><a href="#0">'+name+'</a></h3>';
-		//product price
-		var prodcuthtml = prodcuthtml + '<span class="productprice">$'+price+'</span>';
-		//delete option
-		var prodcuthtml = prodcuthtml + '<a href="#0" class="delete-item">Delete</a>';
-		//actions div
-		var prodcuthtml = prodcuthtml + '<div class="actions">';
-		//quantity label
-		var prodcuthtml = prodcuthtml + '<label for="cd-product-'+ productId +'">Qty</label>';
-		//quantity select
-		var prodcuthtml = prodcuthtml + '<span class="select"><select id="productquantity" name="productquantity">';
-		var i = 0;
-		for (i = 1; i < 11; i++) 
-		{ 
-			if (i == prodcutquantity)
-				var prodcuthtml = prodcuthtml +'<option value="'+i+'" selected>'+i+'</option>';
-
-			else
-				var prodcuthtml = prodcuthtml +'<option value="'+i+'">'+i+'</option>';
-		}
-		
-
-
-		//end of actions div
-		var prodcuthtml = prodcuthtml + '</div>';
-		//end of products details div
-		var prodcuthtml = prodcuthtml + '</div>';
-		//end of product div
-		var prodcuthtml = prodcuthtml + '</div>';
-		//end of li
-		var prodcuthtml = prodcuthtml + '</li>';
-
-		//add it
-		//var productAdded = prodcuthtml;
-		//console.log(prodcuthtml);
-		cartList.prepend(prodcuthtml);
-		
-		
-
-		//updateCart()
-	}
-
-	function removeProduct(product) {		
-		var topPosition = product.offset().top - cartBody.children('ul').offset().top ,
-		productTotPrice = productPrice;
-		product.css('top', topPosition+'px').addClass('deleted');
-		cartTotal.text(0);
-		productQuantity = 0;
-		updateCartCount(true, 0);
-	}
-
-
-	function updateCartCount(emptyCart, quantity) {
-		
-		if ((emptyCart == true) && (quantity == 0))
+		items.each(function()
 		{
-			var actual = 0
-			var next = 1
-			cartCount.find('li').eq(0).text(actual);
-			cartCount.find('li').eq(1).text(next);
-
-		}
-		else
-		{
-
-
-			if( typeof quantity === 'undefined' ) {
-				var actual = Number(cartCount.find('li').eq(0).text()) + 1;
-				var next = actual + 1;
-				
-				if( emptyCart ) {
-					cartCount.find('li').eq(0).text(actual);
-					cartCount.find('li').eq(1).text(next);
-				} else {
-					cartCount.addClass('update-count');
-
-					setTimeout(function() {
-						cartCount.find('li').eq(0).text(actual);
-					}, 150);
-
-					setTimeout(function() {
-						cartCount.removeClass('update-count');
-					}, 200);
-
-					setTimeout(function() {
-						cartCount.find('li').eq(1).text(next);
-					}, 230);
-				}
-			} else {
-				var actual = Number(cartCount.find('li').eq(0).text()) + quantity;
-				var next = actual + 1;
-				
-				cartCount.find('li').eq(0).text(actual);
-				cartCount.find('li').eq(1).text(next);
-			}
-		}
-	}
-
-	function updateCartTotal(price, bool) {
-
-		console.log(cartTotal);
-		bool ? cartTotal.text( (Number(cartTotal.text()) + Number(price)).toFixed(2) )  : cartTotal.text( (Number(cartTotal.text()) - Number(price)).toFixed(2) );
-		//cartTotal.text( (Number(cartTotal.text()) + Number(price)).toFixed(2);
-		//cartTotal.text( (Number(cartTotal.text()) - Number(price)).toFixed(2) );
-
-	}
-
-	function updateSlider(actual, index) {
-		var slider = actual.parent('.cd-customization').prev('a').children('.cd-slider-wrapper'),
-			slides = slider.children('li');
-
-		slides.eq(index).removeClass('move-left').addClass('selected').prevAll().removeClass('selected').addClass('move-left').end().nextAll().removeClass('selected move-left');
-	}
-
-	function resetCustomization(selectOptions) {
-		//close ul.clor/ul.size if they were left open and user is not interacting with them anymore
-		//remove the .hover class from items if user is interacting with a different one
-		selectOptions.siblings('[data-type="select"]').removeClass('is-open').end().parents('.cd-single-item').addClass('hover').parent('li').siblings('li').find('.cd-single-item').removeClass('hover').end().find('[data-type="select"]').removeClass('is-open');
-	}
-		function initCustomization(items) {
-		items.each(function(){
 			var actual = $(this),
 				selectOptions = actual.find('[data-type="select"]'),
 				addToCartBtn = actual.find('.sr-add-to-cart'),
@@ -263,95 +171,184 @@ jQuery(document).ready(function($)
 		});
 	}
 
-	var productPrice = 0
-	var cartWrapper = $('.cd-cart-container');
-	//product id - you don't need a counter in your real project but you can use your real product id
-	var productId = 0;
-	var productCustomization = $('.cd-customization'),
-	cart = $('.cd-cart'),
-	animating = false;
 
-	
-	
-	
-	if( cartWrapper.length > 0 ) {
-		//store jQuery objects
-
-		
-		var cartBody = cartWrapper.find('.body')
-		var cartList = cartBody.find('ul').eq(0);
-		var cartTotal = cartWrapper.find('.checkout').find('span');
-		//console.log(cartTotal);
-		var cartTrigger = cartWrapper.children('.cd-cart-trigger');
-		var cartCount = cartTrigger.children('.count')
-		var addToCartBtn = $('.sr-add-to-cart');
-		var bitcoinback = cartWrapper.find('.bitcoinback');
+	/*
+		This function deals with showing or hiding the cart.
+	*/
+	function toggleCart(bool) 
+	{
+		//always hide the address regardless of the toggle state as we never want to show this unless the 
+		//checkout button has been clicked
+		$('#bitcoinaddresswrapper').hide();
+		//check if the cart is open or not
+		var cartIsOpen = ( typeof bool === 'undefined' ) ? cartWrapper.hasClass('cart-open') : bool;
 		
 		
-
-		//add product to cart
-		addToCartBtn.on('click', function(event){
-			event.preventDefault();
-			addToCart($(this));
-		});
-
-		//open/close cart
-		cartTrigger.on('click', function(event){
-			event.preventDefault();
-			toggleCart();
-		});
-
-		//close cart when clicking on the .cd-cart-container::before (bg layer)
-		cartWrapper.on('click', function(event){
-			if( $(event.target).is($(this)) ) toggleCart(true);
-		});
-
-		//delete an item from the cart
-		cartList.on('click', '.delete-item', function(event){
-			event.preventDefault();
-			removeProduct($(event.target).parents('.product'));
-		});
-
-		cartWrapper.on('click', '.checkout', function(event){
-			event.preventDefault();
-			bitcoinback.addClass('visible');
-			///$('#bitcoin-address-template').show();
-			$('#bitcoinaddresswrapper').show();
-
-			$('#cartlistitems').hide();
-
-
-		});
-
-		
-
-		cartWrapper.on('click', '.bitcoinaddresscopy', function(event){
-			event.preventDefault();
-			/* Get the text field */
-  			var element = document.getElementById("bitcoinaddress");
-  			var $temp = $("<input>");
-  			$("body").append($temp);
-  			$temp.val($(element).text()).select();
- 		 	document.execCommand("copy");
-  			//$temp.remove();
-
-
-		});
-
-
-		cartWrapper.on('click', '#checkoutbitocoin', function(event){
-			event.preventDefault();
+		if( cartIsOpen ) {
+			cartWrapper.removeClass('cart-open');
 			bitcoinback.removeClass('visible');
-			$('#bitcoinaddresswrapper').hide();
-			//$('#bitcoin-address-template').hide();
+			cartList.find('.deleted').remove();
+			bitcoinback.removeClass('visible');
+			$('#bitcoin-address-template').hide();
 			$('#cartlistitems').show();
-		});
 
+			setTimeout(function(){
+				cartBody.scrollTop(0);
+				//check if cart empty to hide it
+				if( Number(cartCount.find('li').eq(0).text()) == 0) cartWrapper.addClass('empty');
+			}, 500);
+		} else {
+			cartWrapper.addClass('cart-open');
+		}
+	}
 
-		
+	function addToCart(trigger) {
+		var cartIsEmpty = cartWrapper.hasClass('empty');
+		//update cart product list
+		addProduct(trigger.data('price'),trigger.data('name'),trigger.data('id'));
+		//update number of items 
+		updateCartCount(cartIsEmpty);
+		//update total price
+		updateCartTotal(trigger.data('price'), true);
+		//show cart
+		cartWrapper.removeClass('empty');
+	}
 
-		
+	function addProduct(price,name,productId) 
+	{
+		//get the product quantity
+		prodcutquantity= $('#productquantity').val();
+		//check if its set or not if not then set it 
+		if (prodcutquantity == undefined)
+			prodcutquantity = 0;
+		//increment it by 1
+		prodcutquantity++;
+		//empty  the car contents
+		cartList.empty();
+		//build the row html
+		//main product div
+		var prodcuthtml = '';
+		var prodcuthtml = prodcuthtml +'<li class="product">';
+		//product image
+		var prodcuthtml = prodcuthtml +'<div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div>';
+		//product details div
+		var prodcuthtml = prodcuthtml + '<div class="product-details">';
+		//product name
+		var prodcuthtml = prodcuthtml + '<h3><a href="#0">'+name+'</a></h3>';
+		//product price
+		var prodcuthtml = prodcuthtml + '<span class="productprice">$'+price+'</span>';
+		//delete option
+		var prodcuthtml = prodcuthtml + '<a href="#0" class="delete-item">Delete</a>';
+		//actions div
+		var prodcuthtml = prodcuthtml + '<div class="actions">';
+		//quantity label
+		var prodcuthtml = prodcuthtml + '<label for="cd-product-'+ productId +'">Qty</label>';
+		//quantity select
+		var prodcuthtml = prodcuthtml + '<span class="select"><select id="productquantity" name="productquantity">';
+		var i = 0;
+		for (i = 1; i < 11; i++) 
+		{ 
+			if (i == prodcutquantity)
+				var prodcuthtml = prodcuthtml +'<option value="'+i+'" selected>'+i+'</option>';
 
+			else
+				var prodcuthtml = prodcuthtml +'<option value="'+i+'">'+i+'</option>';
+		}
+		//end of actions div
+		var prodcuthtml = prodcuthtml + '</div>';
+		//end of products details div
+		var prodcuthtml = prodcuthtml + '</div>';
+		//end of product div
+		var prodcuthtml = prodcuthtml + '</div>';
+		//end of li
+		var prodcuthtml = prodcuthtml + '</li>';
+		//add it
+		//console.log(prodcuthtml);
+		cartList.prepend(prodcuthtml);
+		//updateCart()
+	}
+
+	//remove the product
+	function removeProduct(product) 
+	{		
+		var topPosition = product.offset().top - cartBody.children('ul').offset().top ,
+		productTotPrice = productPrice;
+		product.css('top', topPosition+'px').addClass('deleted');
+		cartTotal.text(0);
+		productQuantity = 0;
+		updateCartCount(true, 0);
+	}
+
+	//update the counter
+	function updateCartCount(emptyCart, quantity) 
+	{
+		//check if cart is empty
+		if ((emptyCart == true) && (quantity == 0))
+		{
+			var actual = 0
+			var next = 1
+			cartCount.find('li').eq(0).text(actual);
+			cartCount.find('li').eq(1).text(next);
+
+		}
+		else
+		{
+			//check if it is the first time the cart is launched
+			if( typeof quantity === 'undefined' ) 
+			{
+				var actual = Number(cartCount.find('li').eq(0).text()) + 1;
+				var next = actual + 1;
+				
+				if( emptyCart ) {
+					cartCount.find('li').eq(0).text(actual);
+					cartCount.find('li').eq(1).text(next);
+				} else {
+					cartCount.addClass('update-count');
+
+					setTimeout(function() {
+						cartCount.find('li').eq(0).text(actual);
+					}, 150);
+
+					setTimeout(function() {
+						cartCount.removeClass('update-count');
+					}, 200);
+
+					setTimeout(function() {
+						cartCount.find('li').eq(1).text(next);
+					}, 230);
+				}
+			} 
+			else 
+			{
+				var actual = Number(cartCount.find('li').eq(0).text()) + quantity;
+				var next = actual + 1;
+				
+				cartCount.find('li').eq(0).text(actual);
+				cartCount.find('li').eq(1).text(next);
+			}
+		}
+	}
+
+	function updateCartTotal(price, bool) 
+	{
+		//console.log(cartTotal);
+		bool ? cartTotal.text( (Number(cartTotal.text()) + Number(price)).toFixed(2) )  : cartTotal.text( (Number(cartTotal.text()) - Number(price)).toFixed(2) );
+		//cartTotal.text( (Number(cartTotal.text()) + Number(price)).toFixed(2);
+		//cartTotal.text( (Number(cartTotal.text()) - Number(price)).toFixed(2) );
+	}
+
+	function updateSlider(actual, index) 
+	{
+		var slider = actual.parent('.cd-customization').prev('a').children('.cd-slider-wrapper'),
+			slides = slider.children('li');
+		slides.eq(index).removeClass('move-left').addClass('selected').prevAll().removeClass('selected').addClass('move-left').end().nextAll().removeClass('selected move-left');
+	}
+
+	function resetCustomization(selectOptions) 
+	{
+		//close ul.clor/ul.size if they were left open and user is not interacting with them anymore
+		//remove the .hover class from items if user is interacting with a different one
+		selectOptions.siblings('[data-type="select"]').removeClass('is-open').end().parents('.cd-single-item').addClass('hover').parent('li').siblings('li').find('.cd-single-item').removeClass('hover').end().find('[data-type="select"]').removeClass('is-open');
 	}
 
 	
