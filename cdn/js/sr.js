1c6f3a051a6a817341f1cb951e4e79a158944cf8
chris <chris@totemsoftware.co.uk>
2018-10-06 00:12:04 +0700

    add cart and close cart funcionality added

--- cdn/js/sr.js
+++ cdn/js/sr.js
@@ -172,20 +172,141 @@ jQuery(document).ready(function($){
 });
 
 */
+
+
+/*
+**=========================
+*START OF GENERIC FUNCTIONS
+*=========================
+*/
+
+function addClass(elements, myClass) {
+
+  // if there are no elements, we're done
+  if (!elements) { return; }
+
+  // if we have a selector, get the chosen elements
+  if (typeof(elements) === 'string') {
+    elements = document.querySelectorAll(elements);
+  }
+
+  // if we have a single DOM element, make it an array to simplify behavior
+  else if (elements.tagName) { elements=[elements]; }
+
+  // add class to all chosen elements
+  for (var i=0; i<elements.length; i++) {
+
+    // if class is not already found
+    if ( (' '+elements[i].className+' ').indexOf(' '+myClass+' ') < 0 ) {
+
+      // add class
+      elements[i].className += ' ' + myClass;
+    }
+  }
+}
+
+function removeClass(elements, myClass) {
+
+  // if there are no elements, we're done
+  if (!elements) { return; }
+
+  // if we have a selector, get the chosen elements
+  if (typeof(elements) === 'string') {
+    elements = document.querySelectorAll(elements);
+  }
+
+  // if we have a single DOM element, make it an array to simplify behavior
+  else if (elements.tagName) { elements=[elements]; }
+
+  // create pattern to find class name
+  var reg = new RegExp('(^| )'+myClass+'($| )','g');
+
+  // remove class from all chosen elements
+  for (var i=0; i<elements.length; i++) {
+    elements[i].className = elements[i].className.replace(reg,' ');
+  }
+}
+
+function changeClassText(elements, value) {
+
+  // if there are no elements, we're done
+  if (!elements) { return; }
+
+  // if we have a selector, get the chosen elements
+  if (typeof(elements) === 'string') {
+    elements = document.querySelectorAll(elements);
+  }
+
+  // if we have a single DOM element, make it an array to simplify behavior
+  else if (elements.tagName) { elements=[elements]; }
+
+  // add class to all chosen elements
+  for (var i=0; i<elements.length; i++) {
+      elements[i].innerHTML = value;
+  }
+}
+
+function hasClass(elements, value) {
+  // if there are no elements, we're done
+  if (!elements) { return; }
+
+  // if we have a selector, get the chosen elements
+  if (typeof(elements) === 'string') {
+    elements = document.querySelectorAll(elements);
+  }
+
+  // if we have a single DOM element, make it an array to simplify behavior
+  else if (elements.tagName) { elements=[elements]; }
+
+  //loop the elements
+  for (var i=0; i<elements.length; i++) {
+  		//check if it is the one
+  		//debug
+  		//console.log(elements[i].className)
+  		//console.log(value);
+  		if (elements[i].className.indexOf(value) !=-1) 
+      	{
+      		return(1);
+      	}
+  }
+  return(0);
+}
+
+function hideClass(elements) 
+{
+	// if there are no elements, we're done
+	if (!elements) { return; }
+
+	// if we have a selector, get the chosen elements
+	if (typeof(elements) === 'string') {
+	elements = document.querySelectorAll(elements);
+	}
+
+	// if we have a single DOM element, make it an array to simplify behavior
+	else if (elements.tagName) { elements=[elements]; }
+
+	//loop the elements
+	for (var i=0; i<elements.length; i++) 
+	{
+		elements[i].style.visibility="hidden";      	
+	}
+}
+
+/*
+*=========================
+*END OF GENERIC FUNCTIONS
+*=========================
+*/
+
+
 var SR = SR || (function(){
 
-	//add to cart element
-	var elem = document.getElementsByClassName('.cd-add-to-cart');
-	document.querySelector('.cd-add-to-cart').addEventListener('click', function () {
-  		//remove empty class
-
-  		//add item to cart
-	});
-
-    var _args = {}; // private
+	var _args = {}; // private
+    var itemcount = 0;
     //set url to production
 	var serverurl = "http://srcryptoapi.eu-west-1.elasticbeanstalk.com/";
-	var cdnurl = '../cdn/';
+	var cdnurl = 'http://s3.eu-west-1.amazonaws.com/srcrypto/';
+	//var cdnurl = '../cdn/';
 	//check if we are local
 	//note : set this whatever your local instance is 127.0.0.1 for example
 	if(window.location.href.indexOf("srcryptowww") > -1) 
@@ -194,13 +315,113 @@ var SR = SR || (function(){
 	}
 	//hold the animating flag
 	var animating = false;
+
+	//add to cart element
+	var elem = document.getElementsByClassName('.cd-add-to-cart');
+	document.querySelector('.cd-add-to-cart').addEventListener('click', function () 
+	{
+		//get details
+		var name = 'lll';
+		var price = '66.88';
+		var productid = 1;
+		var previewpic = '';
+
+		//increment count (quantity)
+  		itemcount = itemcount+1;
+	  	//add item to cart
+	  	var productlist = document.getElementById('cartlistitems');
+		var itemlist  = document.createElement('li');
+		itemlist.className = 'product ';
+		var prodcuthtml = '';
+		//var prodcuthtml = prodcuthtml +'<li class="product">';
+		//product image
+		var prodcuthtml = prodcuthtml +'<div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div>';
+		//product details div
+		var prodcuthtml = prodcuthtml + '<div class="product-details">';
+		//product name
+		var prodcuthtml = prodcuthtml + '<h3><a href="#0">'+name+'</a></h3>';
+		//product price
+		var prodcuthtml = prodcuthtml + '<span class="productprice">$'+price+'</span>';
+		//delete option
+		var prodcuthtml = prodcuthtml + '<a href="#0" class="delete-item">Delete</a>';
+		//actions div
+		var prodcuthtml = prodcuthtml + '<div class="actions">';
+		//quantity label
+		var prodcuthtml = prodcuthtml + '<label for="cd-product-'+ productid +'">Qty</label>';
+		//quantity select
+		var prodcuthtml = prodcuthtml + '<span class="select"><select id="productquantity" name="productquantity">';
+		var i = 0;
+		for (i = 1; i < 11; i++) 
+		{ 
+			if (i == itemcount)
+				var prodcuthtml = prodcuthtml +'<option value="'+i+'" selected>'+i+'</option>';
+
+			else
+				var prodcuthtml = prodcuthtml +'<option value="'+i+'">'+i+'</option>';
+		}
+		//end of actions div
+		var prodcuthtml = prodcuthtml + '</div>';
+		//end of products details div
+		var prodcuthtml = prodcuthtml + '</div>';
+		//end of product div
+		var prodcuthtml = prodcuthtml + '</div>';
+		//end of li
+		var prodcuthtml = prodcuthtml + '</li>';
+		itemlist.innerHTML = prodcuthtml;
+		// append  to the end of theParent
+		productlist.innerHTML = "";
+		productlist.appendChild(itemlist);
+  		
+  		//update counter
+  		changeClassText(document.querySelector('.cd-count'),itemcount);
+
+	});
+
+	//cart clicked element
+	
+	var elem = document.getElementsByClassName('.cd-add-to-cart');
+	document.querySelector('.cd-cart-trigger').addEventListener('click', function () {
+  		//check if cart shoud be shown
+  		//debug
+  		//itemcount = 1;
+  		if (itemcount == 0)
+  		{
+  			//always remove as its 0
+  			removeClass(document.querySelector('.cd-cart-container'),'cart-open');
+  			
+  		}
+  		else
+  		{
+  			//see if the cart is open and toggle it
+  			var res = hasClass(document.querySelector('.cd-cart-container'),'cart-open');
+  			if (res == 1)
+  			{
+  				//close it
+  				removeClass(document.querySelector('.cd-cart-container'),'cart-open');
+  			}
+  			else
+  			{
+  				//hide back element
+  				hideClass(document.querySelector('.bitcoinback'))
+  				//hide btc stuff
+  				hideClass(document.getElementById('bitcoinaddresswrapper'));	
+  				//open it
+  				addClass(document.querySelector('.cd-cart-container'),'cart-open');
+  			}
+  			
+  		}
+  		//add item to cart
+	});
+
+   
 	//init price var
-	var productPrice = 0;
-	var cartWrapper = document.getElementsByClassName('.cd-cart-container');
+	//var productPrice = 0;
+	//var cartWrapper = document.getElementsByClassName('.cd-cart-container');
+
 	//set the cart wrapper
-	var productCustomization = document.getElementsByClassName('.cd-customization');
+	//var productCustomization = document.getElementsByClassName('.cd-customization');
 	//set the cart
-	var cart = document.getElementsByClassName('.cd-cart');
+	//var cart = document.getElementsByClassName('.cd-cart');
 
     return {
         init : function(Args) {
