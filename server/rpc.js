dae0ad2fd89bc507cf7cd8b85bbbb12cdfad9681
chris <chris@totemsoftware.co.uk>
2018-10-12 18:38:33 +0700

    rpc connection

--- server/rpc.js
+++ server/rpc.js
@@ -1,45 +1,27 @@
 
-//load express
 const express = require('express');
 
-var W3CWebSocket = require('websocket').w3cwebsocket;
- 
-//var client = new W3CWebSocket('wss://104.248.68.99:8334/ws', 'echo-protocol');
-var client = new W3CWebSocket('wss://chris:chris@104.248.68.99:8334/ws', {
-  //headers: {
-   // 'Authorization': 'Basic '+new Buffer('chris:chris').toString('base64')
-  //}
-  //,
-  //cert: cert,
-  ///ca: [cert]
+var bitcoin = require('bitcoin');
+var client = new bitcoin.Client({
+  host: 'localhost',
+  port: 18332,
+  user: 'test',
+  pass: 'test'
 });
+
+client.getDifficulty(function(err, difficulty) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('Difficulty: ' + difficulty);
+});
+
  const app = express();
 
-client.onerror = function(e) {
-	console.log(e)
-    console.log('Connection Error');
-};
- 
-client.onopen = function() {
-    console.log('WebSocket Client Connected');
- 
-    
-};
- 
-client.onclose = function() {
-    console.log('echo-protocol Client Closed');
-};
- 
-client.onmessage = function(e) {
-    if (typeof e.data === 'string') {
-        console.log("Received: '" + e.data + "'");
-    }
-};
 
 var port = process.env.PORT || 3000;
-//app.listen( port );
-
-
+app.listen( port );
   
 
 
