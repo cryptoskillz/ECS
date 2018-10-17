8de6d0ced0c3d57750abe01e2610ab85c306aa37
chris <chris@totemsoftware.co.uk>
2018-10-18 04:00:39 +0700

    npm clean up

--- server/node_modules/block_io/test/helpers/cache.js
+++
@@ -1,9 +0,0 @@
-var c = {};
-
-var cache = module.exports = function (k, v) {
-  if (v) {
-    c[k] = v;
-  } else return c[k];
-};
-
-cache.lazy = function (k) { return function () { return cache(k); }};
