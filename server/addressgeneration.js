9e060813f9a6bf6fded50a917639e6c39e7704df
chris <chris@totemsoftware.co.uk>
2018-10-25 10:07:35 +0700

    added block io transaction checker

--- server/addressgeneration.js
+++ server/addressgeneration.js
@@ -4,7 +4,12 @@ const program = require('commander');
 const bitcoin = require("bitcoinjs-lib");
 const bip39 = require('bip39');
 const bip32 = require('bip32')
+//set up block.io
+var BlockIo = require('block_io');
+var version = 2; // API version
+var block_io = new BlockIo(process.env.blockiokey,process.env.blockiosecret, version);
 const network = bitcoin.networks.testnet
+
 program
   .version('0.0.1')
   .description('Generate Address');
@@ -14,6 +19,7 @@ program
   .alias('a')
   .description('generate addresses')
   .action((mnemonic,number) => {
+
     //validate the menmonic
     var res = bip39.validateMnemonic(mnemonic);
     if (res != false)
@@ -22,36 +28,65 @@ program
       const seed = bip39.mnemonicToSeed(mnemonic)
       //set root
       var root = bip32.fromSeed(seed);
-      //debug
-      //console.log(root)
-      
-      //set the counter
-      var i = 0;
-      //start the loop
-      while (i < number)
+
+      //recursive generate address function
+      function generateAddress(root,addressIndex)
       {
+        //console.log(root);
         //user derive path to get a child
-        child = root.derivePath('m/0/'+i);
+        //research
+        child = root.derivePath('m/0/'+addressIndex);
         //debug
         //console.log(child);
 
-        //todo : check if there is any activity in this child and if not store otherwise get the next one in the sequence.
-
-        //get the address
-       
-
-        //var address = bitcoin.payments.p2wpkh({ pubkey: child.publicKey, network }).address
-
-        var address =   bitcoin.payments.p2sh(
-            {redeem: bitcoin.payments.p2wpkh(
-              { pubkey: child.publicKey,network}
-            ),network}
+        //generate a segwit address
+        //research
+        var address =   bitcoin.payments.p2sh
+          (
+            {
+              redeem: bitcoin.payments.p2wpkh
+              (
+                { pubkey: child.publicKey,network}
+              ),network
+            }
           ).address
-        //bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey })
-        //debug
-        console.log(address)
-        i++;
+          //debug
+        //console.log(address)
+        
+        //debug we know this has been sent funs so it will skip this and move the index on
+        //if (addressIndex == 5)
+        //  address = "2Mx1TYm5J87WQAuqo4SBMQZJZLRPNjg716N"
+        
+        //use block.io to see if this address has been used.
+        block_io.get_transactions({'type':'received','address': address}, function (error, data)
+        {
+            //debug
+            //console.log(data)
+            
+            //move on the index
+            addressIndex++;
+            //check for transaxtions
+            if (data.data.txs.length == 0)
+            {
+              //output the address as it has never been used
+              console.log(address)
+              //check if we are under the number of requested addresses and call the function again if this is the case.
+              if (addressIndex <= number)
+                 generateAddress(root,addressIndex)
+            }
+            else
+            {
+              //call the function to get  new address as this one had some transactions. 
+              generateAddress(root,addressIndex)
+            }
+
+        });
       }
+
+      console.log('Generating '+number+' of addresses starting from index 0');
+      //call the generate address function
+      generateAddress(root,0)
+      
     }
   });
 program.parse(process.argv);
