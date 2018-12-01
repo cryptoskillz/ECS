13d7fb67d22f3040d97e42a3b3e08abe665297c3
cryptoskillz <cryptoskillz@protonmail.com>
2018-12-01 19:28:56 +0700

    fixed copy to clipboard

--- cdn/js/sr.js
+++ cdn/js/sr.js
@@ -512,6 +512,33 @@ var SR = SR || (function()
 			
 		});
 
+		//copy the btc address to the clipboard
+		/*
+		cartWrapper.on('click', '.bitcoinaddresscopy', function(event){
+			event.preventDefault();
+  			var element = document.getElementById("bitcoinaddress");
+  			var $temp = $("<input>");
+  			$("body").append($temp);
+  			$temp.val($(element).text()).select();
+ 		 	document.execCommand("copy");
+  			//$temp.remove();
+
+
+		});
+		*/
+
+		document.getElementById('sr-bitcoinaddresscopy').addEventListener('click', function () 
+		{
+			const el = document.createElement('textarea');  // Create a <textarea> element
+			el.value = address;                                 // Set its value to the string that you want copied
+			el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+			el.style.position = 'absolute';                 
+			el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+			document.body.appendChild(el);  
+			el.select();                                    // Select the <textarea> content
+  			document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  			document.body.removeChild(el);      
+		});
 		
 		//shipping and billing checbox
 		document.getElementById('sr-billingandshippingcheck').addEventListener('click', function () 
