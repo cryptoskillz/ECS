cbf6ada34c223bdae17ce929ce91923bfca6120f
cryptoskillz <cryptoskillz@protonmail.com>
2019-08-16 18:02:17 +0700

    refactored sweep function to use simpler sendtosddress function added in 0.16

--- server/api/helpers/api.js
+++ server/api/helpers/api.js
@@ -287,7 +287,7 @@ var api = function() {
 
   /*
 	*
-	*	This function moves a payment to a cold storge address
+	*	This function moves a payment to a cold storge address (admin)
 	*
 	*/
   this.sweep = function sweep(address, res) {
@@ -302,151 +302,60 @@ var api = function() {
       }
       //save the address
       var coldstorageaddress = result.address;
-      //get the sweep address
-      //unlock the wallet
-      client.walletPassphrase(process.env.WALLETPASSPHRASE, 10).then(() => {
-        //get the unspent transaxtions for the address we are intrested in.
-        client.listUnspent(1, 9999999, [address]).then(result => {
-          //debug
-          //console.log('listUnspent')
-          //console.log(result)
-
-          //get the private key
-          client.dumpPrivKey(address).then(pkey => {
-            //debug
-            //console.log('pkey')
-            //console.log(pkey)
-            //console.log(result)
-
-            //check if there are any
-            if (result.length == 0) {
+      client.listUnspent(1, 9999999, [address]).then(result => {
+        //debug
+        console.log(result[0])
+         if (result.length == 0) 
+         {
               //debug
               //console.log(result);
-
-              //exit gracefully
               res.send(
                 JSON.stringify({
                   result: "nothing to sweep no unspent transactions"
                 })
               );
               return;
-            } else {
-              //debug
-              //console.log(result[0])
-
-              //check the confirmation count
-              //note it is set to 1 for now as I want to play with it as soon as possible.  It should 3 - 6 when we are happy
-              if (result[0].confirmations >= process.env.CONFIRMATIONS) {
-                //estimate fee
-                client.estimateSmartFee(6).then(fee => {
+          } 
+          else 
+          {
+            if (result[0].confirmations >= process.env.CONFIRMATIONS) 
+            {
+                amounttosend = result[0].amount.toFixed(8);
+                //debug
+                console.log('ams'+amounttosend);
+                //return;
+                client.sendToAddress(coldstorageaddress,amounttosend).then(result => {
                   //debug
-                  //console.log('fee')
-                  //console.log(fee)
+                  console.log('result');
+                  console.log(result);
 
-                  //work out the amount to send
-                  var amounttosend = result[0].amount - fee.feerate;
-                  amounttosend = amounttosend.toFixed(8);
-                  //debug
-                  //console.log(amounttosend)
-                  //return
-
-                  //create raw transaction
-                  /*
-      		          we are in a catch 22 here 
-      		          Unhandled rejection RpcError: signrawtransaction is deprecated and will be fully removed in v0.18. To use signrawtransaction in v0.17,
-      		          restart bitcoind with -deprecatedrpc=signrawtransaction.
-      		          Projects should transition to using signrawtransactionwithkey and signrawtransactionwithwallet before upgrading to v0.18
-      		          but v0.17 does not support signrawtransactionwithkey so we wil update when v0.18 comes out
-
-      		          Innputs
-
-      		          txid: the transation id you want to use as your input (from listUnspent)
-      		          vout: the transaciton id to you want to use as your input (from listUnspent)
-
-      		          Output
-
-      		          address to send to
-      		          amount to send      
-    		          */
-
-                  //console.log(coldstorageaddress)
-                  client
-                    .createRawTransaction(
-                      [{ txid: result[0].txid, vout: result[0].vout }],
-                      [{ [coldstorageaddress]: amounttosend }]
-                    )
-                    .then(txhash => {
-                      //debug
-                      //console.log('txhash');
-                      //console.log(txhash)
-
-                      //sign it
-                      //note may have to trap for errors
-                      client
-                        .signRawTransaction(
-                          txhash,
-                          [
-                            {
-                              txid: result[0].txid,
-                              vout: result[0].vout,
-                              amount: result[0].amount,
-                              scriptPubKey: result[0].scriptPubKey,
-                              redeemScript: result[0].redeemScript
-                            }
-                          ],
-                          [pkey]
-                        )
-                        .then(signed => {
-                          //debug
-                          //console.log('signed');
-                          //console.log(signed);
-
-                          //broadcast it
-                          //note may have to trap for errors
-                          client
-                            .sendRawTransaction(signed.hex)
-                            .then(broadcasted => {
-                              //debug
-                              //console.log('broadcasted');
-                              //console.log(broadcasted);
-
-                              //build sql
-                              let sqldata = ["1", address];
-                              let sql = `UPDATE sessions
-	                        SET swept = ?
-	                        WHERE address = ?`;
-
-                              //run sql
-                              db.run(sql, sqldata, function(err) {
-                                if (err) {
-                                }
-                                //update the address in cold storage so it is not used again.
-                                //build sql
-                                let sqldata = ["1", coldstorageaddress];
-                                let sql = `UPDATE ecs_coldstorageaddresses
-	                          SET used = ?
-	                          WHERE ecs_coldstorageaddress = ?`;
-                            //console.log(coldstorageaddress)
-                            //console.log(sql)
-
-                                //run sql
-                                db.run(sql, sqldata, function(err) {
-                                  if (err) {
-                                  }
-                                  //lock wallet
-                                  client.walletLock();
-                                  //return status
-                                  res.send(JSON.stringify({ status: "swept" }));
-                                  return;
-                                });
-                              });
-                            });
-                        });
+                  let sqldata = ["1", address];
+                  let sql = `UPDATE sessions
+                  SET swept = ?
+                  WHERE address = ?`;
+                  //run sql
+                  db.run(sql, sqldata, function(err) {
+                    if (err) {
+                    }
+                    //update the address in cold storage so it is not used again.
+                    //build sql
+                    let sqldata = ["1", coldstorageaddress];
+                    let sql = `UPDATE ecs_coldstorageaddresses
+                               SET used = ?
+                              WHERE ecs_coldstorageaddress = ?`;
+                    //run sql
+                    db.run(sql, sqldata, function(err) {
+                      if (err) {
+                      }
+                      //return status
+                      res.send(JSON.stringify({ status: "swept" }));
+                      return;
                     });
+                  });
                 });
-              } else {
-                //lock wallet
-                client.walletLock();
+              }
+              else
+              {
                 //return status
                 res.send(
                   JSON.stringify({
@@ -458,9 +367,7 @@ var api = function() {
               }
             }
           });
-        });
-      });
-    });
+       });
+    };
   };
-};
-exports.api = api;
+  exports.api = api;
